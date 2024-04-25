3572b07eda9fbe8229a9cd9af59d76941581c30d
mmontan0 <165187154+mmontan0@users.noreply.github.com>
2024-04-25 21:05:04 +0000

    pagina

--- app.rb
+++ app.rb
@@ -39,9 +39,29 @@ get ("/random/new") do
   end
 
 get ("/random/results")do
-  @min_rand=params.fetch("users_number_root").to_f
+  @min_rand=params.fetch("users_min").to_i
 
-  @max_rand=@the_num_root**0.5
+  @max_rand=params.fetch("users_max").to_i
+
+  @result_rand=rand(@min_rand..@max_rand)
   
   erb(:random_results)
+end
+
+get ("/payment/new") do
+  erb(:payment_new_calc)
+  end
+
+get ("/payment/results")do
+  @apr_=params.fetch("users_apr").to_f
+
+  @years_=params.fetch("users_years").to_f
+
+  @present_value=params.fetch("users_pv").to_f
+
+
+
+  @result_payment=(@apr_*@present_value)/(1-(1+@apr_)**-@years_)
+  
+  erb(:payment_results)
 end
